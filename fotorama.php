00527af2b26fc1dc146958d61700e506a16975f7
artpolikarpov <artpolikarpov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-25 11:49:10 +0000

    Upgrade to the 4.4.6
    git-svn-id: https://plugins.svn.wordpress.org/fotorama/trunk@793542 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fotorama.php
+++ fotorama.php
@@ -8,7 +8,7 @@ Plugin Name: Fotorama
 Plugin URI: http://fotorama.io/
 Description: Transforms standard galleries into fotoramas. Fotorama is a simple, stunning, powerful jQuery gallery. Licensed under the MIT.
 Author: Artem Polikarpov
-Version: 4.4.5
+Version: 4.4.6
 Author URI: https://github.com/artpolikarpov
 */
 
